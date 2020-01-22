0fcc0ebce6373625640621ca9ff29c7ae464d552
XiaoweiGu <xw.gu@hotmail.com>
2020-01-22 12:26:36 +0000

    Update and rename Exp1.m to Demo.m

--- Demo.m
+++ Demo.m
@@ -2,7 +2,7 @@ clear all
 clc
 close all
 
-%%
+%% Loading Autompg data
 % load autompg_train;
 % load autompg_test;
 % TRAIN=autompg_train;
@@ -11,7 +11,7 @@ close all
 % data0 = TRAIN(:,1:6);
 % Y1 = TEST(:,7);
 % data1 = TEST(:,1:6);
-%%
+%% Loading Delta Ailerons data
 % load deltaailerons_train;
 % load deltaailerons_test;
 % TRAIN=deltaailerons_train;
@@ -20,7 +20,7 @@ close all
 % data0 = TRAIN(:,1:5);
 % Y1 = TEST(:,6);
 % data1 = TEST(:,1:5);
-%%
+%% Loading Triazines data
 % load triazines_train;
 % load triazines_test;
 % TRAIN=triazines_train;
@@ -29,7 +29,7 @@ close all
 % data0 = TRAIN(:,1:60);
 % Y1 = TEST(:,61);
 % data1 = TEST(:,1:60);
-%%
+%% Loading Autos data
 load autos_train;
 load autos_test;
 TRAIN=autos_train;
@@ -38,7 +38,7 @@ Y0= TRAIN(:,16);
 data0 = TRAIN(:,1:15);
 Y1 = TEST(:,16);
 data1 = TEST(:,1:15);
-%%
+%% Loading Calhousing data
 % load calhousing_train;
 % load calhousing_test;
 % TRAIN=calhousing_train;
@@ -49,17 +49,18 @@ data1 = TEST(:,1:15);
 % data1 = TEST(:,1:8);
 
 
-Input.MaxIt=200;
-Input.nPop=100;
-Input.w=0.7298;
-Input.cs=1.49618;
-Input.cp=1.49618;
-Input.c=1.49618;
-Input.wdamp=1;
-Input.data0=data0;
-Input.data1=data1;
-Input.Y0=Y0;
-Input.Y1=Y1;
+Input.data0=data0; % training input
+Input.data1=data1; % testing input
+Input.Y0=Y0; % training output
+Input.Y1=Y1; % testing output
+
+Input.MaxIt=200; % maximum iteration number for PSO algorithm
+Input.nPop=100; % population number for PSO algorithm
+Input.w=0.7298; % inertia weight for PSO algorithm
+Input.cs=1.49618; % c1 for PSO algorithm
+Input.cp=1.49618; % c2 for PSO algorithm
+Input.wdamp=1; % damping coefficient for PSO algorithm
+
 [Output]=PSOALFS(Input);
-Prediction=Output.PreSeq;
-Output.RMSE
+Prediction=Output.PreSeq; % predicted output 
+Output.RMSE % RMSE of the prediction
